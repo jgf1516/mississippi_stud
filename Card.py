df2ae70ae62194ed96728eb97176ea1b3d649b3e
jgf1516 <jgf1516@gmail.com>
2022-03-18 20:26:06 -0400

    Finished Card class, created constructor for Deck and ordered_deck

--- Card.py
+++ Card.py
@@ -7,7 +7,32 @@ class Card:
         self.rank= Card.index_to_rank[int(index/4)]
         self.suit= Card.index_to_suit[index%4]
     def __repr__(self):
-        return "{rank} of {suit}".format(rank=self.rank, suit=self.suit)
-card1=Card(41)
-print(card1)
+        #First, find corresponding suit unicode
+        self.suit_char=""
+        if self.suit=="Spades":
+            self.suit_char="\u2660"
+        elif self.suit=="Hearts":
+            self.suit_char="\u2661"
+        elif self.suit=="Diamonds":
+            self.suit_char="\u2662"
+        else:
+            self.suit_char="\u2663"
+        #Then print the rank, then the suit, then a space
+        return "{rank}{suit} ".format(rank=self.rank, suit=self.suit_char)
+    def card_to_index(self):
+        ri=0
+        si=0
+        for r in Card.index_to_rank:
+            if Card.index_to_rank[r]==self.rank:
+                ri=r
+                break
+        for s in Card.index_to_suit:
+            if Card.index_to_suit[s]==self.suit:
+                si=s
+                break
+        return int(4 * ri + si)
+#some testing:
+#card1=Card(40)
+#print(card1)
+#print(card1.card_to_index())
 
--- Deck.py
+++ Deck.py
@@ -1,4 +1,4 @@
-import Card
+from Card import Card
 import random
 class Deck:
     def __init__(self, ind1, ind2, ind3, ind4, ind5):
@@ -7,3 +7,15 @@ class Deck:
         self.card3=Card(ind3)
         self.card4=Card(ind4)
         self.card5=Card(ind5)
+    def __repr__(self):
+        return self.card1.__repr__()+self.card2.__repr__()+self.card3.__repr__()+self.card4.__repr__()+self.card5.__repr__()
+    def ordered_deck(self):
+        indices=[self.card1.card_to_index(), self.card2.card_to_index(), self.card3.card_to_index(), self.card4.card_to_index(), self.card5.card_to_index()]
+        ordered_indices=sorted(indices)
+        return Deck(ordered_indices[0],ordered_indices[1],ordered_indices[2],ordered_indices[3],ordered_indices[4])
+
+
+deck=Deck(34, 35, 20, 1, 15)
+print(deck)
+deck2=deck.ordered_deck()
+print(deck2)
